caa8e61796b3562f634b187957b9f2fe3145435c
agrajj <>
2023-09-04 22:51:25 +0530

    feat: add logo.jpeg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { environment } from '../environments/environment';
+import { VERSION } from '@angular/core';
+const prod = (path: string) =>
+  environment.production
+    ? `${environment.cdn}@${VERSION.full}/dist/assets/${path}`
+    : path;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'neevindore-in';
+  logo = prod(`images/logo.jpeg`);
 }
