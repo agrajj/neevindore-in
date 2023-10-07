2cd63849449b0d23bb6b0eb162787b8d6ada44f5
Agraj <>
2023-10-08 00:58:13 +0530

    release: v0.1.32

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,30 @@ import { TranslocoService } from '@ngneat/transloco';
 export class AppComponent {
   selectedLanguage: string = localStorage.getItem('language') || 'en';
 
-  constructor(private translocoService: TranslocoService) {}
+  heroImages = [
+    // `url(../img/main.jpg)`,
+    `url(../img/main1.jpeg)`,
+    // `url(../img/main2.jpeg)`,
+    // // `url(../img/main3.jpeg)`,
+    // `url(../img/main4.jpeg)`,
+    // `url(../img/main5.jpeg)`,
+  ];
+  heroImageIndex = 0;
+
+  mainImage: string = this.heroImages[0];
+
+  constructor(private translocoService: TranslocoService) {
+    this.heroImageRotator();
+  }
+
+  heroImageRotator() {
+    setInterval(() => {
+      this.heroImageIndex =
+        this.heroImageIndex === this.heroImages.length - 1
+          ? 0
+          : this.heroImageIndex + 1;
+    }, 5000);
+  }
 
   onChange(target: any) {
     this.translocoService.setActiveLang(target.value);
